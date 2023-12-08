a608e36cb7c2dc02910744f9d346238610659f6e
jGulzat <gulzatj97@mail.ru>
2023-12-08 19:31:26 +0600

    Initial Commit

---
+++ ios22/common/FinishViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FinishViewController.swift
+//  ios22
+//
+//  Created by Gulzat Zheenbek kyzy on 8/12/23.
+//
+
+import UIKit
+
+class FinishViewController: UIViewController {
+
+    var data: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
---
+++ ios22/common/SecondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.swift
+//  ios22
+//
+//  Created by Gulzat Zheenbek kyzy on 8/12/23.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+}
